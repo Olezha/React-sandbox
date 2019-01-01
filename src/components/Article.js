b6a6b802039bcc3ca628b90f1424b743992f9d02
Oleh Shklyar <oleh.shklyar@gmail.com>
2019-01-01 03:27:49 +0200

    simple click

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,13 +6,20 @@ function Article(props) {
 
     return (
         <div>
-            <h2>{article.title}</h2>
+            <h2>
+                {article.title}
+                <button onClick={handleClick}>close</button>
+            </h2>
             <section>{article.text}</section>
             <p>
                 created: {(new Date(article.date)).toDateString()}
             </p>
         </div>
     )
+}
+
+function handleClick() {
+    console.log('clicked');
 }
 
 export default Article
