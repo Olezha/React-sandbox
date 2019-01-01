5050ae0cf4394bf2c22425c676953a2a86ea48d3
Oleh Shklyar <oleh.shklyar@gmail.com>
2019-01-01 11:28:26 +0200

    list

--- src/articles.js
+++ src/articles.js
@@ -10,5 +10,23 @@ export default [
         "title": "Article 2",
         "text": " text Article 2 text",
         "date": "2018-12-30T14:20:00.000Z"
+    },
+    {
+        "id": "3",
+        "title": "Article 3",
+        "text": " text Article 3 text",
+        "date": "2019-01-01T10:40:00.000Z"
+    },
+    {
+        "id": "4",
+        "title": "Article 4",
+        "text": " text Article 4 text",
+        "date": "2019-01-01T10:40:00.000Z"
+    },
+    {
+        "id": "5",
+        "title": "Article 5",
+        "text": " text Article 5 text",
+        "date": "2019-01-01T10:50:00.000Z"
     }
 ]
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import Article from './Article'
+import ArticleList from './ArticleList'
 import articles from '../articles'
 
 function App() {
-
     const adv = <section>some adv</section>;
 
     return (
         <div className="hello" style={{color: 'cadetblue'}}>
             <h1>Hello World!</h1>
             {adv}
-            <Article article={articles[0]} foo="bar" flag/>
-            <Article article={articles[1]}/>
+            <ArticleList articles={articles}/>
         </div>
     )
 }
