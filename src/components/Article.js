824c2a5972d4f46529ee545e59b3f91f60ff5c1a
Oleh Shklyar <oleh.shklyar@gmail.com>
2019-01-01 15:37:45 +0200

    handle event from parent

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,33 +1,27 @@
 import React, {Component} from 'react'
-// import React, {PureComponent} from 'react'
 
 class Article extends Component {
-// class Article extends PureComponent {
 
     // constructor
 
-    state = {
-        isOpen: true
-    };
-
-    componentWillMount() {
-        console.log('Article WillMount');
-    }
+    // componentWillMount() {
+    //     console.log('Article WillMount');
+    // }
 
     // render
 
-    componentDidMount() {
-        console.log('Article DidMount');
-    }
+    // componentDidMount() {
+    //     console.log('Article DidMount');
+    // }
 
     // componentWillReceiveProps(nextProps) {
     //     // if (nextProps.isOpen !== this.props.isOpen) {}
     // }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        // return nextProps !== this.props || nextState !== this.state;
-        return nextState !== this.state;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     // return nextProps !== this.props || nextState !== this.state;
+    //     return nextState !== this.state;
+    // }
 
     componentWillUpdate(nextProps, nextState) {
         console.log('Article WillUpdate', nextProps, nextState)
@@ -36,21 +30,20 @@ class Article extends Component {
     // componentWillUnmount() {}
 
     render() {
-        const {article} = this.props;
-        const open = this.state.isOpen && <section><p>open</p></section>;
+        const {article, isOpen} = this.props;
+        const text = isOpen && <section>{article.text}</section>;
 
         return (
             <div className="card">
                 <div className="card-header">
                     <button onClick={this.handleClick}
                             className="btn btn-primary float-right">
-                        {this.state.isOpen ? 'close' : 'open'}
+                        {isOpen ? 'close' : 'open'}
                     </button>
                     <h2>{article.title}</h2>
                 </div>
                 <div className="card-body">
-                    {open}
-                    <section>{article.text}</section>
+                    {text}
                     <p>created: {(new Date(article.date)).toDateString()}</p>
                 </div>
             </div>
@@ -58,9 +51,9 @@ class Article extends Component {
     }
 
     handleClick = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
+        // this.setState({
+        //     isOpen: !this.state.isOpen
+        // })
     }
 }
 
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -4,21 +4,33 @@ import './style.css'
 
 class ArticleList extends PureComponent {
 
+    state = {
+        openArticleId: null
+    };
+
     render() {
         console.log(3);
 
         const articleElements = this.props.articles.map((article, index) =>
-            <li key={article.id} className="article-list__li">
+            <li onClick={this.handleClick.bind(this, article.id)}
+                key={article.id} className="article-list__li">
                 <p>{index}</p>
-                <Article article={article}/>
+                <Article article={article}
+                         isOpen={this.state.openArticleId === article.id}/>
+                {console.log(this.state.openArticleId === article.id, this.state.openArticleId, article.id)}
             </li>
         );
 
         return (
-            <ul>
-                {articleElements}
-            </ul>
+            <ul>{articleElements}</ul>
         )
+    }
+
+    handleClick = openArticleId => {
+        console.log(openArticleId);
+        this.setState({
+            openArticleId: openArticleId
+        })
     }
 }
 
