cf96b6f954dc9c5ac7ea0176be08c1962f973724
Oleh Shklyar <oleh.shklyar@gmail.com>
2019-02-01 22:54:36 +0200

    preview of an image upload

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -2,10 +2,33 @@ import React, {Component} from 'react';
 
 class ImageUploader extends Component {
 
+    state = {
+        image: 'https://via.placeholder.com/150'
+    };
+
+    imageSelected = e => {
+        this.setState({
+            image: URL.createObjectURL(e.target.files[0])
+        })
+    };
+
     render = () => {
+        let {image} = this.state;
+
         return (
             <div>
                 <h2>Image Uploader</h2>
+
+                <img src={image} style={{width: '150px'}} alt="image"/>
+
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="formControlImage">Select image to upload:</label>
+                        <input type="file" accept="image/*"
+                               id="formControlImage" className="form-control-file"
+                               onChange={this.imageSelected}/>
+                    </div>
+                </form>
             </div>
         )
     }
