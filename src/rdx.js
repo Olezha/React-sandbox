83f48e18d3e2cc463609b8259833c3c086345761
Oleh <pio_ua@ukr.net>
2019-02-07 22:03:57 +0200

    1

--- src/rdx.js
+++ src/rdx.js
@@ -7,15 +7,23 @@ const composeEnhancers = process.env.NODE_ENV !==
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
 const initialState = {
-    course: 'React.js'
+    course: 'React.js',
+    studentsCount: 0
 };
 
 function reduser(state = initialState, action) {
     switch (action.type) {
-        case 'CHANGE_COURSE': return {
-            ...state,
-            course: action.course
-        };
+        case 'CHANGE_COURSE':
+            return {
+                ...state,
+                course: action.course
+            };
+
+        case 'ADD_STUDENT':
+            return {
+                ...state,
+                studentsCount: ++state.studentsCount
+            };
 
         default: return state;
     }
@@ -32,3 +40,13 @@ const ChangeCourseToAngular = {
 store.dispatch(ChangeCourseToAngular);
 
 console.log(store.getState());
+
+const studentsCount = document.getElementById('studentsCount');
+
+const render = () => {
+    studentsCount.innerText = store.getState().studentsCount;
+};
+store.subscribe(render);
+
+store.dispatch({type: 'ADD_STUDENT'});
+store.dispatch({type: 'ADD_STUDENT'});
